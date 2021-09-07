71e255c9be4b267c2948e0ed13aa0bccc2aafb3e
harwinder <harwinder@zapbuild.com>
2021-09-07 16:23:21 +0530

    ck-editor implementation

--- ck-editor-four/src/app/app.component.ts
+++ ck-editor-four/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ck-editor-four';
+
+    /* Ckeditor  */
+    public config = {
+      uiColor: '#F0F3F4',
+      height: '200',
+      toolbarGroups: [
+        { name: 'basicstyles', groups: ['basicstyles'] },
+        { name: 'links' },
+        { name: 'insert' },
+        { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align'] },
+        /*
+        { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+        { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+
+        { name: 'insert' },
+        // { name: 'tools' },
+        { name: 'others' },
+
+        { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+        { name: 'styles' },
+        { name: 'colors' }*/
+      ]
+    };
 }
